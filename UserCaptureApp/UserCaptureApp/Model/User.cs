213b5aa2f8df56518845efa6bf362958b1b3eac7
Samkelo Siyabonga Ngubo <samke704@gmail.com>
2024-04-21 22:26:52 +0200

    Create a simple capture form to capture multiple users names, surnames and cellphone numbers and store the data in an XML file. You should be able to list all the users and edit/delete an individual user. Bonus points for validation, UI, OO principles
    Create a simple capture form to capture multiple users names,
    surnames and cellphone numbers and store the data in an XML file.
    You should be able to list all the users and edit/delete an individual user.
    Bonus points for validation, UI, OO principles

---
+++ UserCaptureApp/UserCaptureApp/Model/User.cs
@@ -0,0 +1,10 @@
+﻿namespace UserCaptureApp.Model
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Cellphone { get; set; }
+    }
+
+}
